d4e940a5c7e458e685c8c0f08724368d389513a0
Gokulraja2004 <gg7537007@gmail.com>
2026-02-23 20:11:56 +0530

    it is for last commit

---
+++ Environments/dev/main.tf
@@ -0,0 +1,19 @@
+module "vpc" {
+  source = "../../modules/vpc"
+  env ="dev"
+  cidr_block = "10.0.0.0/16"
+  subnet_cidr = "10.0.1.0/24"
+  AZ = "${var.aws_region}a"
+
+  
+}
+
+module "ec2" {
+    source = "../../modules/ec2"
+    ami_id = "ami-051a31ab2f4d498f5"
+    instance_type = "t3.micro"
+    subnet_id = module.vpc.subnet_id
+    count = 1
+    env = "dev"
+  
+}
---
+++ Environments/dev/variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {
+    type = string
+    default = "ap-south-1"
+    description = "it is for region"
+  
+}
