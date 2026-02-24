14957201f0345d47f6f7bedbf302fc50b9c238a8
Gokulraja2004 <gg7537007@gmail.com>
2026-02-24 10:22:03 +0530

    this hcxnn

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     =var.subnet_id
-  count         =var.count
+  count         =var.instance_count
 
   tags = {
     Name = "${var.env}-ec2-${count.index}"
